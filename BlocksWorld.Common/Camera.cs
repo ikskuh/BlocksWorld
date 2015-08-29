b597151d2ba3b439775621e8d4ed68062ea5d274
Felix Queißner <git@mq32.de>
2015-08-29 09:34:08 +0200

    Missed Camera.cs

---
+++ BlocksWorld.Common/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenTK;
+using Jitter.Dynamics;
+
+namespace BlocksWorld
+{
+    public abstract class Camera
+    {
+        public abstract Matrix4 CreateProjectionMatrix(float aspect);
+
+        public abstract Matrix4 CreateViewMatrix();
+
+        public float ZNear { get; set; } = 0.01f;
+
+        public float ZFar { get; set; } = 10000.0f;
+    }
+}
